e87fb7550d346ea55c159353988add6df09fcd9f
Austin <austncoe@gmail.com>
2026-08-24 18:26:12 -0400

    Initialize Supabase client with URL and anon key

--- supabase.js
+++ supabase.js
@@ -1,1 +1,9 @@
+const SUPABASE_URL = "https://uvpmmbioerejeyybfntb.supabase.co";
 
+const SUPABASE_ANON_KEY = "sb_publishable_CmcUD2ze8lhj4HvlMfoYiQ_DGG_xabb";
+
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
