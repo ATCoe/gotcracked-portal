46f96ade866c5fc17b987e4092cdf17552a39fe6
Austin <austncoe@gmail.com>
2026-08-27 21:44:58 -0400

    Harden Marlon approval RPC privileges

---
+++ supabase/migrations/20260828_harden_marlon_owner_approval_execute_privileges.sql
@@ -0,0 +1,8 @@
+﻿revoke all on function public.decide_marlon_high_level_change(uuid,boolean) from public, anon;
+grant execute on function public.decide_marlon_high_level_change(uuid,boolean) to authenticated;
+
+revoke all on function public.decide_marlon_ui_update(uuid,boolean) from public, anon;
+grant execute on function public.decide_marlon_ui_update(uuid,boolean) to authenticated;
+
+revoke all on function public.decide_marlon_change_approval(uuid,boolean) from public, anon;
+grant execute on function public.decide_marlon_change_approval(uuid,boolean) to authenticated;
