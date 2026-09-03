4d67f3f11e3e02d07d5ef541a51b1f6e7ee4728d
Austin <austncoe@gmail.com>
2026-09-02 21:11:51 -0400

    Bump runtime for store selector UI

--- portal-runtime-loader.js
+++ portal-runtime-loader.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const VERSION = '20260902-staff-permissions3';
+  const VERSION = '20260902-store-selector-ui1';
   const ACCOUNT_PAGE_VERSION = '20260826-account-page2';
   const SALES_OPS_VERSION = '20260827-reconciliation2';
   const APPOINTMENTS_VERSION = '20260827-production9';
